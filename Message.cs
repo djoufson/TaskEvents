a97b2cd31c1a0c679f58a603a34db7fb2de9c00c
Djoufson CHE BENE <djouflegran@gmail.com>
2023-12-03 04:28:33 +0100

    code: Edit the Message payload class

---
+++ Message.cs
@@ -0,0 +1,10 @@
+﻿using CommunityToolkit.Mvvm.Messaging.Messages;
+
+namespace TaskEvents;
+
+public class Message : ValueChangedMessage<string>
+{
+    public Message(string value) : base(value)
+    {
+    }
+}
