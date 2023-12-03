dc4e25d90e41c568aef35762dc2eab075255122a
Djoufson CHE BENE <djouflegran@gmail.com>
2023-12-03 03:36:39 +0100

    code: Link the view to the viewmodel

--- MainPage.xaml.cs
+++ MainPage.xaml.cs
@@ -2,24 +2,9 @@
 
 public partial class MainPage : ContentPage
 {
-	int count = 0;
-
-	public MainPage()
+	public MainPage(ViewModel vm)
 	{
+		BindingContext = vm;
 		InitializeComponent();
 	}
-
-	private void OnCounterClicked(object sender, EventArgs e)
-	{
-		count++;
-
-		if (count == 1)
-			CounterBtn.Text = $"Clicked {count} time";
-		else
-			CounterBtn.Text = $"Clicked {count} times";
-
-		SemanticScreenReader.Announce(CounterBtn.Text);
-	}
 }
-
-
